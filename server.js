0e1606c1ada83193ae507ed9cc65e89f24cabedd
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-04-30 13:03:11 -0400

    Changed public to build in server to get back on heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname,'client/public')));
+app.use(express.static(path.join(__dirname,'client/build')));
 
 
 app.use(routes);
